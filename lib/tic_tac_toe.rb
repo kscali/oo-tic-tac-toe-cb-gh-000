de3e570917a189d7ad9a4a440b16553f00cbc2e9
Koy Saeteurn <kosatun@aol.com>
2018-07-14 04:06:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,13 +35,13 @@ class TicTacToe
   end
   
   
-  def position_taken?
+  def position_taken?(index)
     @board[index]== "X" || @board[index] == "O"
   end
   
   
   def valid_move?
-    index.between?(0,8) && !position_taken?
+    index.between?(0,8) && !position_taken?(index)
   end
   
   
