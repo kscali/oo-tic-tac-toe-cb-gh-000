f99e722458ef169ab6dc6c7ca16e27750486afc7
Koy Saeteurn <kosatun@aol.com>
2018-07-14 03:33:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,7 +68,13 @@ class TicTacToe
   end
   
   
-  
+  def won?(@board)
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(@board, combo[0])
+    end 
+  end
   
   
   
