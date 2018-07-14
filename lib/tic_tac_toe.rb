2a4555ac65bb77b89f4bb5023c203faa23dc773b
Koy Saeteurn <kosatun@aol.com>
2018-07-14 04:16:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,6 +14,8 @@ class TicTacToe
     [0,4,8],
     [6,4,2]
 ]
+
+  
   
   
   def display_board
@@ -24,16 +26,7 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
-  def play
-    while !over?
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner(@board)}!"
-    elsif draw?
-      puts "Cat's Game!"
-    end
-  end
+  
   def input_to_index(user_input)
     user_input.to_i - 1
   end
