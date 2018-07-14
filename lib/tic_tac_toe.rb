2ca57413cd8d01db6d5851d026351c934c5dfffd
Koy Saeteurn <kosatun@aol.com>
2018-07-14 03:27:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,8 +40,8 @@ class TicTacToe
   end
   
   
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(@board, index)
+    index.between?(0,8) && !position_taken?(@board, index)
   end
   
   
